208f1383af450f4b8b5b6432f230b6352e490a8f
Mauricio Sánchez <msanchez@aplyca.com>
2019-03-29 10:48:26 -0500

    Adding Network resources

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,26 @@
+# Create VPC
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "Training"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Terraform"
+  }
+}
+
 resource "aws_instance" "training" {
   ami           = "${var.app["ami"]}"
   instance_type = "t2.nano"
+    subnet_id = "${aws_subnet.main.id}"
 
   tags = {
     Name = "Training Terraform"
